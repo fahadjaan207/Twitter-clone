f4d22259f510179feef63ed6873a73d2f693855a
fahadjaan207 <93815070+fahadjaan207@users.noreply.github.com>
2023-09-15 23:11:19 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -23,15 +23,7 @@ const connect = () => {
 };
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+
 
 app.use(cookieParser());
 app.use(express.json());
